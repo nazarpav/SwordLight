3891f41079bb0dd940048e53d9104c64c4780aba
Brick <powerzieeee@gmail.com>
2021-04-20 23:01:48 +0300

    Adaptive canvas + window

---
+++ Assets/src/UI/MainWindow/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchLevel(int levelNumber)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelNumber);
+    }
+}
