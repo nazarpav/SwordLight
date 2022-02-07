288b947b6d668be083b427e28692331f9964e5d3
PLAYNET\pavliuk-n <nazarii.pavliuk@zagravagames.com>
2022-02-07 16:09:53 +0200

    Переробка під звичайний платформер.

--- Assets/src/GameLogic/Player/PlayerController.cs
+++ Assets/src/GameLogic/Player/PlayerController.cs
@@ -8,14 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public GameObject StartPosition;
-    public Collider2D LayerTop;
-    public Collider2D LayerMiddle;
-    public Collider2D LayerBottom;
-    public float PosYOnTop;
-    public float PosYOnMiddle;
-    public float PosYOnBottom;
     public Rigidbody2D rigidbody;
-    public Vector2 StartVelocity;
+    public float RunVelocity;
+    public float MaxVelocity;
     public Vector2 ForceToUp;
     public Vector2 ForceToJump;
     public Vector2 ForceToFallDown;
@@ -24,71 +19,15 @@ public class PlayerController : MonoBehaviour
     public string OnRunTriggerName;
     public string OnMakeDamageTriggerName;
     public string OnLayerMovementTriggerName;
+    private bool faceRight = true;
 
     public RectTransform progressBar;
     public WLayers CurrentLayer { set; get; }
     public WLayers StartLayer;
     private bool _isOnGround;
-    private void SetLayerOnStart()
-    {
-        if (StartLayer < TMP_CONSTANTS.minLayer || StartLayer > TMP_CONSTANTS.maxLayer)
-        {
-            CurrentLayer = WLayers.Medium;
-        }
-        else
-        {
-            CurrentLayer = StartLayer;
-        }
-        switch (CurrentLayer)
-        {
-            case WLayers.Bottom:
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnBottom, gameObject.transform.position.z);
-                break;
-            case WLayers.Medium:
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnMiddle, gameObject.transform.position.z);
-                break;
-            case WLayers.Top:
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnTop, gameObject.transform.position.z);
-                break;
-        }
-        UpdateLayersOn_Off();
-    }
-    private void UpdateLayersOn_Off()
-    {
-        animator.SetTrigger(OnLayerMovementTriggerName);
-        LayerTop.enabled = false;
-        LayerMiddle.enabled = false;
-        LayerBottom.enabled = false;
-        switch (CurrentLayer)
-        {
-            case WLayers.Bottom:
-                LayerBottom.enabled = true;
-                break;
-            case WLayers.Medium:
-                LayerMiddle.enabled = true;
-                break;
-            case WLayers.Top:
-                LayerTop.enabled = true;
-                break;
-        }
-    }
-    private void UpdateRuntimeLayerUp()
-    {
-        if (CurrentLayer < TMP_CONSTANTS.maxLayer)
-        {
-            ++CurrentLayer;
-            UpdateLayersOn_Off();
-        }
-    }
-    private void UpdateRuntimeLayerDown()
-    {
-        --CurrentLayer;
-        UpdateLayersOn_Off();
-    }
     void Start()
     {
         InitInput();
-        SetLayerOnStart();
         ForceToUp.x = 0;
         OnGameRestart_TMP();
     }
@@ -110,13 +49,26 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        StartVelocity.y = rigidbody.velocity.y;
-        rigidbody.velocity = StartVelocity;
+        //StartVelocity.y = rigidbody.velocity.y;
+        //rigidbody.velocity = StartVelocity;
 
         if (progressBar.rect.width <= 0)
         {
             SceneManager.LoadScene("scev");
         }
+
+        float moveX = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(Mathf.Clamp(rigidbody.velocity.x + moveX * RunVelocity * Time.deltaTime, -MaxVelocity, MaxVelocity), rigidbody.velocity.y);
+        if (moveX > 0 && !faceRight)
+            flip();
+        else if (moveX < 0 && faceRight)
+            flip();
+    }
+
+    private void flip()
+    {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
     private void OnLevelWin()
     {
@@ -170,10 +122,8 @@ public class PlayerController : MonoBehaviour
     }
     void InitInput()
     {
-        GameInput.instance.OnSwipeDown += OnMoveDown;
-        GameInput.instance.OnSwipeLeft += OnJump;
-        GameInput.instance.OnSwipeRight += OnForceFallDown;
-        GameInput.instance.OnSwipeUp += OnMoveUp;
+        GameInput.instance.OnSwipeUp += OnJump;
+        GameInput.instance.OnSwipeDown += OnForceFallDown;
         GameInput.instance.OnTap += OnAttack;
     }
     void OnForceFallDown()
@@ -186,53 +136,11 @@ public class PlayerController : MonoBehaviour
     }
     void OnMoveUp()
     {
-        if (CurrentLayer < TMP_CONSTANTS.maxLayer)
-        {
-            UpdateRuntimeLayerUp();
-            if (_isOnGround)
-            {
-                switch (CurrentLayer)
-                {
-                    case WLayers.Bottom:
-                        gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnBottom, gameObject.transform.position.z);
-                        break;
-                    case WLayers.Medium:
-                        gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnMiddle, gameObject.transform.position.z);
-                        break;
-                    case WLayers.Top:
-                        gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnTop, gameObject.transform.position.z);
-                        break;
-                }
-                //rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0.0f);
-                //rigidbody.AddForce(ForceToUp);
-                //Invoke("UpdateRuntimeLayerUp", 0.5f);
-            }
-        }
+
     }
     void OnMoveDown()
     {
-        if (CurrentLayer > TMP_CONSTANTS.minLayer)
-        {
-            UpdateRuntimeLayerDown();
-            if (_isOnGround)
-            {
-                switch (CurrentLayer)
-                {
-                    case WLayers.Bottom:
-                        gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnBottom, gameObject.transform.position.z);
-                        break;
-                    case WLayers.Medium:
-                        gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnMiddle, gameObject.transform.position.z);
-                        break;
-                    case WLayers.Top:
-                        gameObject.transform.position = new Vector3(gameObject.transform.position.x, PosYOnTop, gameObject.transform.position.z);
-                        break;
-                }
-                //rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0.0f);
-                ////rigidbody.AddForce(-ForceToUp/2);
-                //UpdateRuntimeLayerDown();
-            }
-        }
+
     }
     void OnJump()
     {
